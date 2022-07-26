c774e71bfe420cb258e6dad67df3765eed4c6fe7
Joe Taylor <joe@princeton.edu>
2022-07-26 13:16:32 -0400

    Update comments in NetworkMessage.hpp to include ARRL DIGI contest as a Special Operating Activity.

--- Network/NetworkMessage.hpp
+++ Network/NetworkMessage.hpp
@@ -197,8 +197,9 @@
  *       3 -> FIELD DAY
  *       4 -> RTTY RU
  *       5 -> WW DIGI
- *       6 -> FOX
- *       7 -> HOUND
+ *       6 -> ARRL DIGI
+ *       7 -> FOX
+ *       8 -> HOUND
  *
  *    The Frequency Tolerance  and T/R period fields may  have a value
  *    of  the maximum  quint32 value  which implies  the field  is not
