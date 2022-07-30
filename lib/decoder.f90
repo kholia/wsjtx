9c67aa818245ac1e6d0bb67bec38aa402c5e14c8
Joe Taylor <joe@princeton.edu>
2022-07-30 09:58:50 -0400

    Remove a diagnostic print statement.

--- lib/decoder.f90
+++ lib/decoder.f90
@@ -617,7 +617,6 @@ contains
 1001 format(i6.6,i4,f5.1,i5,' ~ ',1x,a37,1x,a2)
     if(ios13.eq.0) write(13,1002) params%nutc,nint(sync),snr,dt,freq,0,decoded0
 1002 format(i6.6,i4,i5,f6.1,f8.0,i4,3x,a37,' FT8')
-    print*,'A',ios13
 
     if(ncontest.eq.6) then
        i1=index(decoded0,' ')
