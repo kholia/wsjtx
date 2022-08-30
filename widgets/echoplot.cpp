66b90904cfc06e99ac6a6b9339de1fc5900a106a
Joe Taylor <joe@princeton.edu>
2022-08-30 15:23:51 -0400

    Better orthogonality of Gain and Zero functions in EchoPlot.

--- widgets/echoplot.cpp
+++ widgets/echoplot.cpp
@@ -127,7 +127,7 @@ void EPlotter::draw()                           //draw()
     painter2D.setPen(penBlue);
     j=0;
     for(i=0; i<m_w; i++) {
-      y = 0.9*m_h2 - gain*(m_h/10.0)*(blue[i0+i]-1.0) - 0.01*m_h2*m_plotZero;
+      y = 0.9*m_h2 - gain*(m_h/10.0)*(blue[i0+i]) - 0.01*m_h2*m_plotZero;
       LineBuf[j].setX(i);
       LineBuf[j].setY(y);
       j++;
@@ -145,7 +145,7 @@ void EPlotter::draw()                           //draw()
 
   j=0;
   for(int i=0; i<m_w; i++) {
-    y = 0.9*m_h2 - gain*(m_h/10.0)*(red[i0+i]-1.0) - 0.01*m_h2*m_plotZero;
+    y = 0.9*m_h2 - gain*(m_h/10.0)*(red[i0+i]) - 0.01*m_h2*m_plotZero;
     LineBuf[j].setX(i);
     LineBuf[j].setY(y);
     j++;
