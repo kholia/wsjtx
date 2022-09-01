ca04a2f3bbdb9d886234b85d2708ca8afd7de6eb
Uwe Risse <dg2ycb@gmx.de>
2022-09-01 09:54:01 +0200

    A bit more time to test RC4.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -1061,9 +1061,9 @@ void MainWindow::not_GA_warning_message ()
   MessageBox::critical_message (this,
                                 "This is a pre-release version of WSJT-X 2.6.0 made\n"
                                 "available for testing purposes.  By design it will\n"
-                                "be nonfunctional after Nov 30, 2022.");
+                                "be nonfunctional after Dec 31, 2022.");
   auto now = QDateTime::currentDateTimeUtc ();
-  if (now >= QDateTime {{2022, 11, 30}, {23, 59, 59, 999}, Qt::UTC}) {
+  if (now >= QDateTime {{2022, 12, 31}, {23, 59, 59, 999}, Qt::UTC}) {
     Q_EMIT finished ();
   }
 }
