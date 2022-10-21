f4e8ee4f75240075889d1d6dbab7983cd132b1cb
Steven Franke <s.j.franke@icloud.com>
2022-10-21 13:47:12 -0500

    Improve ft8 decoding on crowded bands.

--- lib/ft8_decode.f90
+++ lib/ft8_decode.f90
@@ -44,7 +44,7 @@ contains
 
     class(ft8_decoder), intent(inout) :: this
     procedure(ft8_decode_callback) :: callback
-    parameter (MAXCAND=300,MAX_EARLY=100)
+    parameter (MAXCAND=500,MAX_EARLY=100)
     real*8 tsec,tseq
     real s(NH1,NHSYM)
     real sbase(NH1)
