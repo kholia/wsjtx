f216648c05330656e2d82c5f7ed0e9f61fcf1e36
Joe Taylor <joe@princeton.edu>
2022-12-04 09:21:57 -0500

    Remove forced nfreq=144 in astro.cpp.

--- map65/astro.cpp
+++ map65/astro.cpp
@@ -63,7 +63,7 @@ void Astro::astroUpdate(QDateTime t, QString mygrid, QString hisgrid,
   int isec=sec;
   double uth=nhr + nmin/60.0 + sec/3600.0;
   int nfreq=(int)datcom_.fcenter;
-  if(nfreq<10 or nfreq > 50000) nfreq=144;
+//  if(nfreq<10 or nfreq > 50000) nfreq=144;
 
   astrosub_(&nyear, &month, &nday, &uth, &nfreq, mygrid.toLatin1(),
             hisgrid.toLatin1(), &azsun, &elsun, &azmoon, &elmoon,
