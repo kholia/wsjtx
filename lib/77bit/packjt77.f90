6f3ae5190d2cd7063c7379626c34ea6b72df1da0
Uwe Risse <dg2ycb@gmx.de>
2022-12-29 11:07:49 +0100

    Allow some more exchanges for FT RU contest mode.

--- lib/77bit/packjt77.f90
+++ lib/77bit/packjt77.f90
@@ -205,7 +205,7 @@ subroutine unpack77(c77,nrx,msg,unpk77_success)
 ! be used in place of a callsign from the hashtable
 !
   parameter (NSEC=85)      !Number of ARRL Sections
-  parameter (NUSCAN=65)    !Number of US states and Canadian provinces
+  parameter (NUSCAN=171)    !Number of US states and Canadian provinces
   parameter (MAXGRID4=32400)
   integer*8 n58
   integer ntel(3)
@@ -244,7 +244,18 @@ subroutine unpack77(c77,nrx,msg,unpk77_success)
        "NM ","NY ","NC ","ND ","OH ","OK ","OR ","PA ","RI ","SC ",  &
        "SD ","TN ","TX ","UT ","VT ","VA ","WA ","WV ","WI ","WY ",  &
        "NB ","NS ","QC ","ON ","MB ","SK ","AB ","BC ","NWT","NF ",  &
-       "LB ","NU ","YT ","PEI","DC "/
+       "LB ","NU ","YT ","PEI","DC ","DR ","FR ","GD ","GR ","OV ",  &
+       "ZH ","ZL ","X01","X02","X03","X04","X05","X06","X07","X08",  &
+       "X09","X10","X11","X12","X13","X14","X15","X16","X17","X18",  &
+       "X19","X20","X21","X22","X23","X24","X25","X26","X27","X28",  &
+       "X29","X30","X31","X32","X33","X34","X35","X36","X37","X38",  &
+       "X39","X40","X41","X42","X43","X44","X45","X46","X47","X48",  &
+       "X49","X50","X51","X52","X53","X54","X55","X56","X57","X58",  &
+       "X59","X60","X61","X62","X63","X64","X65","X66","X67","X68",  &
+       "X69","X70","X71","X72","X73","X74","X75","X76","X77","X78",  &
+       "X79","X80","X81","X82","X83","X84","X85","X86","X87","X88",  &
+       "X89","X90","X91","X92","X93","X94","X95","X96","X97","X98",  &
+       "X99"/
   data dxcall13_set/.false./
   data mycall13_set/.false./
   data mycall13_0/''/
@@ -1194,7 +1205,7 @@ subroutine pack77_3(nwords,w,i3,n3,c77)
 !             - DX:     rpt serial          R 559 0013
 ! Example message:  TU; W9XYZ K1ABC R 579 MA           1 28 28 1 3 13   74
   
-  parameter (NUSCAN=65)    !Number of US states and Canadian provinces/territories
+  parameter (NUSCAN=171)    !Number of US states and Canadian provinces/territories
   character*13 w(19)
   character*77 c77
   character*6 bcall_1,bcall_2
@@ -1208,7 +1219,18 @@ subroutine pack77_3(nwords,w,i3,n3,c77)
        "NM ","NY ","NC ","ND ","OH ","OK ","OR ","PA ","RI ","SC ",  &
        "SD ","TN ","TX ","UT ","VT ","VA ","WA ","WV ","WI ","WY ",  &
        "NB ","NS ","QC ","ON ","MB ","SK ","AB ","BC ","NWT","NF ",  &
-       "LB ","NU ","YT ","PEI","DC "/
+       "LB ","NU ","YT ","PEI","DC ","DR ","FR ","GD ","GR ","OV ",  &
+       "ZH ","ZL ","X01","X02","X03","X04","X05","X06","X07","X08",  &
+       "X09","X10","X11","X12","X13","X14","X15","X16","X17","X18",  &
+       "X19","X20","X21","X22","X23","X24","X25","X26","X27","X28",  &
+       "X29","X30","X31","X32","X33","X34","X35","X36","X37","X38",  &
+       "X39","X40","X41","X42","X43","X44","X45","X46","X47","X48",  &
+       "X49","X50","X51","X52","X53","X54","X55","X56","X57","X58",  &
+       "X59","X60","X61","X62","X63","X64","X65","X66","X67","X68",  &
+       "X69","X70","X71","X72","X73","X74","X75","X76","X77","X78",  &
+       "X79","X80","X81","X82","X83","X84","X85","X86","X87","X88",  &
+       "X89","X90","X91","X92","X93","X94","X95","X96","X97","X98",  &
+       "X99"/
 
   if(w(1)(1:1).eq.'<' .and. w(2)(1:1).eq.'<') go to 900
   if(nwords.eq.4 .or. nwords.eq.5 .or. nwords.eq.6) then
