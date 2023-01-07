210ee2147e73f868523b1fa018b055c4256f61b2
Joe Taylor <joe@princeton.edu>
2023-01-07 13:17:56 -0500

    Use snprintf in two places, replacing calls to deprecated sprintf().

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -4819,9 +4819,9 @@ void MainWindow::guiUpdate()
     if(m_transmitting) {
       char s[42];
       if(SpecOp::FOX==m_specOp and ui->tabWidget->currentIndex()==1) {
-        sprintf(s,"Tx:  %d Slots",foxcom_.nslots);
+        snprintf(s,sizeof(s),"Tx:  %d Slots",foxcom_.nslots);
       } else {
-        sprintf(s,"Tx: %s",msgsent);
+        snprintf(s,sizeof(s),"Tx: %s",msgsent);
       }
       m_nsendingsh=0;
       if(s[4]==64) m_nsendingsh=1;
