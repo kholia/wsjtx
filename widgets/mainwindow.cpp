0a139e5acdc9df75ce6f3ad1d24f7ce441a821f0
Uwe Risse <dg2ycb@gmx.de>
2023-02-13 14:05:18 +0100

    Don't Tx when the frequency is manually entered in the band select box and the Enter/Return key is pressed.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -7530,6 +7530,7 @@ void MainWindow::on_bandComboBox_activated (int index)
   m_bandEdited = true;
   band_changed (frequency);
   m_wideGraph->setRxBand (m_config.bands ()->find (frequency));
+  auto_tx_mode(false);
 }
 
 void MainWindow::band_changed (Frequency f)
