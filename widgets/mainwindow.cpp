d08a056c8ec7fc6194e1450f94598168e6a264f7
Uwe Risse <dg2ycb@gmx.de>
2023-03-22 12:05:04 +0100

    Call FoxReset(); only when in Fox mode because it erases the decodedTextBrowser.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -7837,9 +7837,8 @@ void MainWindow::band_changed (Frequency f)
       }
     setRig (f);
     setXIT (ui->TxFreqSpinBox->value ());
-
-    // when changing bands, don't preserve the Fox queues
-    FoxReset("BandChange");
+    m_specOp=m_config.special_op_id();
+    if (m_specOp==SpecOp::FOX) FoxReset("BandChange");  // when changing bands, don't preserve the Fox queues
   }
 }
 
