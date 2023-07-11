68aae125887b78b7ce8eb8d33b196d4d131efa7a
Uwe Risse <dg2ycb@gmx.de>
2023-07-11 21:46:51 +0200

    Automatic detection of the version number for the splash screen.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -1091,7 +1091,7 @@ MainWindow::MainWindow(QDir const& temp_directory, bool multiple,
 void MainWindow::not_GA_warning_message ()
 {
   MessageBox::critical_message (this,
-                                "This is a pre-release version of WSJT-X 2.7.0-rc2 made\n"
+                                "This is a pre-release version of WSJT-X " + version (false) + " made\n"
                                 "available for testing purposes.  By design it will\n"
                                 "be nonfunctional after Jan 15, 2024.");
   auto now = QDateTime::currentDateTimeUtc ();
