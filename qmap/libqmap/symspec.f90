c02b977c9423087d48c8bf7f52976daf417ab380
Joe Taylor <joe@princeton.edu>
2023-12-04 12:49:17 -0500

    Remove references to nfast in symspec.f90.

--- qmap/libqmap/symspec.f90
+++ qmap/libqmap/symspec.f90
@@ -26,7 +26,6 @@ subroutine symspec(k,ndiskdat,nb,nbslider,nfsample,    &
   data rms/999.0/,k0/99999999/,nadjx/0/,nadjy/0/
   save
 
-  nfast=1
   if(k.gt.5751000) go to 999
   if(k.lt.NFFT) then
      ihsym=0
@@ -37,8 +36,7 @@ subroutine symspec(k,ndiskdat,nb,nbslider,nfsample,    &
      w2a=0.
      w2b=0.
      do i=1,NFFT
-        w(i)=(sin(i*pi/NFFT))**2                          !Window for nfast=1
-        if(i.lt.17833) w2a(i)=(sin(i*pi/17832.925))**2    !Window a for nfast=2
+        w(i)=(sin(i*pi/NFFT))**2                          !Window
         j=i-8916
         if(j.gt.0 .and. j.lt.17833) w2b(i)=(sin(j*pi/17832.925))**2    ! b
      enddo
@@ -112,31 +110,21 @@ subroutine symspec(k,ndiskdat,nb,nbslider,nfsample,    &
 
   cx00=cx
 
-  do mm=1,nfast
-     ihsym=ihsym+1
-     if(nfast.eq.1) then
-        cx=w*cx00                           !Apply window for 2nd forward FFT
-     else
-        if(mm.eq.1) then
-           cx=w2a*cx00
-        else
-           cx=w2b*cx00
-        endif
-     endif
-
-     call four2a(cx,NFFT,1,1,1)          !Second forward FFT (X)
-
-     n=min(322,ihsym)
-     do i=1,NFFT
-        sx=real(cx(i))**2 + aimag(cx(i))**2  
-        ss(n,i)=sx                    ! Pol = 0
-        savg(i)=savg(i) + sx
-        ssz5a(i)=sx
-     enddo
+  ihsym=ihsym+1
+  cx=w*cx00                           !Apply window for 2nd forward FFT
+  call four2a(cx,NFFT,1,1,1)          !Second forward FFT (X)
+  n=min(322,ihsym)
+  do i=1,NFFT
+     sx=real(cx(i))**2 + aimag(cx(i))**2
+     ss(n,i)=sx                    ! Pol = 0
+     savg(i)=savg(i) + sx
+     ssz5a(i)=sx
   enddo
 
   nkhz=nint(1000.d0*(fcenter-int(fcenter)))
   if(fcenter.eq.0.d0) nkhz=125
+!  write(*,3001) hsym,ts,ja,jb,ihsym
+!3001 format(2f12.3,3i10)
 
 999 return
 end subroutine symspec
