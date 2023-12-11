c413ea2f149f86ed3cd4887e55dd9b5463b745e6
Joe Taylor <joe@princeton.edu>
2023-12-11 10:51:50 -0500

    Remove the 'QMAP deciding finished' stuff, it was a bad idea.

--- qmap/mainwindow.cpp
+++ qmap/mainwindow.cpp
@@ -938,7 +938,6 @@ void MainWindow::guiUpdate()
   if(decodes_.ndecodes > m_fetched) {
     while(m_fetched<decodes_.ndecodes) {
       QString t=QString::fromLatin1(decodes_.result[m_fetched]);
-      if(t.trimmed()=="QMAP decoding finished") break;
       if(m_UTC0!="" and m_UTC0!=t.left(4)) {
         t1="-";
         ui->decodedTextBrowser->append(t1.repeated(60));
