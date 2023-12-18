8be6d702d2884e0f1062d397e5bc217a4df7614d
Joe Taylor <joe@princeton.edu>
2023-12-18 13:28:03 -0500

    Several corrections in cfom_iq.f90.

--- qmap/libqmap/cfom_iq.f90
+++ qmap/libqmap/cfom_iq.f90
@@ -2,19 +2,19 @@ program cfom_iq
 
   parameter(NMAX=60*96000)
   integer*2 id2(2,NMAX)
-  complex c,w,wstep
-  real*8 fcenter,uth8,twopi
+  complex*16 c,w,wstep
+  real*8 fcenter,uth8,twopi,dphi
   character*6 mygrid
 
   twopi=8.d0*atan(1.d0)
-  open(10,file='231028_0140.iq',status='old',access='stream')
-  open(12,file='231028_0140.cfom',status='unknown',access='stream')
+  open(10,file='231028_0131.iq',status='old',access='stream')
+  open(12,file='231028_0131.cfom',status='unknown',access='stream')
 
   mygrid='FN20OG'
   nyear=2023
   month=10
   nday=28
-  uth8=01 + 40.d0/60
+  uth8=01 + 31.d0/60
   nfreq=1296
   call astrosub00(nyear,month,nday,uth8,nfreq,mygrid,ndop0)
   call astrosub00(nyear,month,nday,uth8+1.d0/60.d0,nfreq,mygrid,ndop1)
@@ -29,17 +29,17 @@ program cfom_iq
   j=0
   w=1.0
   do isec=1,60
-     dop=dop0 + (i-0.5)*(dop1-dop0)/60.
-     dphi=-dop*twopi/96000.0
+     dop=dop0 + (isec-0.5)*(dop1-dop0)/60.
+     dphi=dop*twopi/96000.0
      wstep=cmplx(cos(dphi),sin(dphi))
      do n=1,96000
         j=j+1
         x=id2(1,j)
         y=id2(2,j)
         w=w*wstep
-        c=100.0*w*cmplx(x,y)
-        id2(1,j)=0.01*real(c)
-        id2(2,j)=0.01*aimag(c)
+        c=100.d0*w*cmplx(x,y)
+        id2(1,j)=0.01d0*real(c)
+        id2(2,j)=0.01d0*aimag(c)
      enddo
   enddo
 
