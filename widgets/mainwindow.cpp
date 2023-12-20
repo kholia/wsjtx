50bd14937f9803259a3b4fa6c4e836086a5fb02e
Uwe Risse <dg2ycb@gmx.de>
2023-12-20 15:26:06 +0100

    Preparations for Release Candidate 3.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -1110,9 +1110,9 @@ void MainWindow::not_GA_warning_message ()
   MessageBox::critical_message (this,
                                 "This is a pre-release version of WSJT-X " + version (false) + " made\n"
                                 "available for testing purposes.  By design it will\n"
-                                "be nonfunctional after Jan 15, 2024.");
+                                "be nonfunctional after September 30, 2024.");
   auto now = QDateTime::currentDateTimeUtc ();
-  if (now >= QDateTime {{2024, 01, 15}, {23, 59, 59, 999}, Qt::UTC}) {
+  if (now >= QDateTime {{2024, 09, 30}, {23, 59, 59, 999}, Qt::UTC}) {
     Q_EMIT finished ();
   }
 }
