fada229d83df9c9cd5498e4607e41626ef459c76
Joe Taylor <joe@princeton.edu>
2024-02-22 14:35:41 -0500

    Input ts (approx tsync) from command line.

--- lib/superfox/sfox_mod.f90
+++ lib/superfox/sfox_mod.f90
@@ -5,7 +5,7 @@ module sfox_mod
   integer isync(50)
 
 contains
-  subroutine sfox_init(mm0,nn0,kk0,itu,fspread,delay,fsample)
+  subroutine sfox_init(mm0,nn0,kk0,itu,fspread,delay,fsample,ts)
 
     character*2 itu
     integer isps(54)
@@ -25,11 +25,10 @@ contains
     ND2=NN-ND1          !Data symbols after sync 
     NFZ=3               !First zero
 
-    tsync=3.0
-    jsps=nint((12.8-tsync)*fsample/NN)
+    jsps=nint((12.8-ts)*fsample/NN)
     iloc=minloc(abs(isps-jsps))
     NSPS=isps(iloc(1))  !Samples per symbol
-    NS=nint(tsync*fsample/NSPS)
+    NS=nint(ts*fsample/NSPS)
     if(mod(NS,2).eq.1) NS=NS+1
     NSYNC=NS*NSPS       !Samples in sync waveform
     NZ=NSPS*(NN+NS)     !Samples in full Tx waveform
