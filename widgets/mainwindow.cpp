d4a5ea60e6355325e520d590e99d8754bf30b9a8
Uwe Risse <dg2ycb@gmx.de>
2024-03-04 10:00:03 +0100

    Preparations for RC4.

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -1110,9 +1110,9 @@ void MainWindow::not_GA_warning_message ()
   MessageBox::critical_message (this,
                                 "This is a pre-release version of WSJT-X " + version (false) + " made\n"
                                 "available for testing purposes.  By design it will\n"
-                                "be nonfunctional after September 30, 2024.");
+                                "be nonfunctional after October 30, 2024.");
   auto now = QDateTime::currentDateTimeUtc ();
-  if (now >= QDateTime {{2024, 9, 30}, {23, 59, 59, 999}, Qt::UTC}) {
+  if (now >= QDateTime {{2024, 10, 30}, {23, 59, 59, 999}, Qt::UTC}) {
     Q_EMIT finished ();
   }
 }
