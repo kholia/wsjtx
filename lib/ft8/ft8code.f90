d1d0d8a0d0693f91e30b35f59039f509bd9be481
Joe Taylor <joe@princeton.edu>
2024-08-14 12:23:44 -0400

    Remove unused part of the ft8code "Usage" message.  Thanks to Pengo, VK3PGO.

--- lib/ft8/ft8code.f90
+++ lib/ft8/ft8code.f90
@@ -23,7 +23,7 @@ program ft8code
           'LDPC(174,91) encoding,'
      print*,'bit and symbol ordering, and other details of the FT8 protocol.'
      print*
-     print*,'Usage: ft8code [-c grid] "message"  # Results for specified message'
+     print*,'Usage: ft8code "message"  # Results for specified message'
      print*,'       ft8code -T                   # Examples of all message types'
      print*,'       ft8code -t                   # Short format examples'
      go to 999
